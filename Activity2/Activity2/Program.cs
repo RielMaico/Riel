b6b44e82ab64dba735a949acdf625c81bb1feb34
unknown <Admin@CSLAB217.CSSERVER.local>
2024-02-06 20:05:16 +0800

    act2

--- Activity2/Activity2/Program.cs
+++ Activity2/Activity2/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Activity2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Transactions;
+
+namespace Activity2
+{
+    public class Car
+    {
+
+        public string Brand
+        {
+
+            get;
+            set;
+        }
+
+        public string Color
+        {
+            get;
+            set;
+        }
+
+        public int Price
+        {
+            get;
+            set;
+        }
+
+    }
+}
+
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        List<Car> list = new List<Car>();
+
+        while (true)
+        {
+            Console.WriteLine("Enter new data: Y/N");
+
+            if (Console.ReadLine() == "N")
+            {
+                break;
+            }
+
+            Car car1 = new Car();
+
+            Console.Write("Enter Car Brand: ");
+            car1.Brand = Console.ReadLine();
+
+            Console.Write("Enter Car Color: ");
+            car1.Color = Console.ReadLine();
+
+            Console.Write("Enter Car Price: ");
+            car1.Price = int.Parse(Console.ReadLine());
+
+            list.Add(car1);
+
+        }
+
+        list.ForEach(x =>
+        {
+            Console.WriteLine("\n" + x.Brand);
+            Console.WriteLine(x.Color);
+            Console.WriteLine(x.Price);
+            Console.WriteLine("\n");
+
+        });
+      }
+
+    }
+
+
+
+
+
+
